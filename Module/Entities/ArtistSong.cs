15db20078661378e2236da4e4010d8523cabdf29
Korenivskyi, Danylo <d.korenivskiy@gmail.com>
2022-02-11 22:11:28 +0200

    Module -done

---
+++ Module/Entities/ArtistSong.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module.Entities
+{
+    public class ArtistSong
+    {
+        public int ArtistId { get; set; }
+        public Artist Artist { get; set; }
+        public int SongId { get; set; }
+        public Song Song { get; set; }
+    }
+}
